5c8da1bba226b956faadd89677ff864662f20d8b
Jahid Hasan <theshakil53@gmail.com>
2022-07-06 10:12:58 +0600

    style home page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
-
-import './App.css';
+import "./App.css"
+import {  Routes, Route } from "react-router-dom";
+import Header from "./components/header/Header"
+import HomePages from "./home/HomePages";
 
 function App() {
   return (
     <div className="App">
-       <h1>hello</h1>
+  
+         <Header />
+         <Routes>
+         <Route exact path='/' element={<HomePages />} />
+         </Routes>
     </div>
   );
 }
---
+++ src/home/HomePages.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Homes from "../components/homes/Homes"
+
+function HomePages() {
+  return (
+    <div>
+      <Homes />
+    </div>
+  )
+}
+
+export default HomePages
